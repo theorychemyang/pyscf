theorychemyang/pyscf

commit 6fe9523b278ab82f97d4032ea71fa887b29255ba

NEO: support fractional number of nuclei and electrons
By inputting `q_nuc_occ`, we can now control the occupation numbers of
quantum nuclei. At the same time, the number of electrons will also be
adjusted to make the system neutral (or keep the same charge as when it
was set in the beginning).

pyscf/neo/mole.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 import os
-import math
+import numpy
 from pyscf import gto
 from pyscf.lib import logger
 
@@ -29,7 +29,7 @@ class Mole(gto.mole.Mole):
         self.elec = None # a Mole object for NEO-electron and classical nuclei
         self.nuc = [] # a list of Mole objects for quantum nuclei
 
-    def build_nuc_mole(self, atom_index):
+    def build_nuc_mole(self, atom_index, frac=None):
         '''
         Return a Mole object for specified quantum nuclei.
 
@@ -49,14 +49,14 @@ class Mole(gto.mole.Mole):
             basis = gto.basis.parse(open(os.path.join(dirnow, 'basis/s-pb4d.dat')).read())
         elif self.atom_pure_symbol(atom_index) == 'H':
             basis = gto.basis.parse(open(os.path.join(dirnow, 'basis/pb4d.dat')).read())
-            #alpha = 2 * math.sqrt(2) * self.mass[atom_index]
-            #beta = math.sqrt(2)
+            #alpha = 2 * numpy.sqrt(2) * self.mass[atom_index]
+            #beta = numpy.sqrt(2)
             #n = 8
             #basis = gto.expand_etbs([(0, n, alpha, beta), (1, n, alpha, beta), (2, n, alpha, beta)])
         else:
             # even-tempered basis
-            alpha = 2 * math.sqrt(2) * self.mass[atom_index]
-            beta = math.sqrt(3)
+            alpha = 2 * numpy.sqrt(2) * self.mass[atom_index]
+            beta = numpy.sqrt(3)
             n = 12
             basis = gto.expand_etbs([(0, n, alpha, beta), (1, n, alpha, beta), (2, n, alpha, beta)])
             #logger.info(self, 'Nuclear basis for %s: n %s alpha %s beta %s' %(self.atom_symbol(atom_index), n, alpha, beta))
@@ -75,9 +75,12 @@ class Mole(gto.mole.Mole):
         nuc.spin = 0
         nuc.nelectron = 2
 
+        # fractional
+        nuc.nnuc = frac
+
         return nuc
 
-    def build(self, quantum_nuc='all', **kwargs):
+    def build(self, quantum_nuc='all', q_nuc_occ=None, **kwargs):
         '''assign which nuclei are treated quantum mechanically by quantum_nuc (list)'''
         super().build(self, **kwargs)
 
@@ -109,15 +112,36 @@ class Mole(gto.mole.Mole):
         self.elec.super_mol = self
         self.elec.build(**kwargs)
 
+        # deal with fractional number of nuclei
+        if q_nuc_occ is not None:
+            q_nuc_occ = numpy.array(q_nuc_occ)
+            if q_nuc_occ.size != self.nuc_num:
+                raise ValueError('q_nuc_occ must match the dimension of quantum_nuc')
+            unocc = numpy.ones_like(q_nuc_occ) - q_nuc_occ
+            unocc_Z = 0
+            idx = 0
         # set all quantum nuclei to have zero charges
         quantum_nuclear_charge = 0
         for i in range(self.natm):
             if self.quantum_nuc[i] is True:
                 quantum_nuclear_charge -= self.elec._atm[i, 0]
+                if q_nuc_occ is not None:
+                    unocc_Z += unocc[idx] * self.elec._atm[i, 0]
+                    idx += 1
                 self.elec._atm[i, 0] = 0 # set nuclear charges of quantum nuclei to 0
         self.elec.charge += quantum_nuclear_charge # charge determines the number of electrons
+        if q_nuc_occ is not None:
+            # remove excessive electrons to make the system neutral
+            self.elec.charge += numpy.floor(unocc_Z)
+            self.elec.nhomo = 1.0 - (unocc_Z - numpy.floor(unocc_Z))
+        else:
+            self.elec.nhomo = None
 
         # build a list of Mole objects for quantum nuclei
+        if q_nuc_occ is None:
+            q_nuc_occ = [None] * self.nuc_num
+        idx = 0
         for i in range(self.natm):
             if self.quantum_nuc[i] == True:
-                self.nuc.append(self.build_nuc_mole(i))
+                self.nuc.append(self.build_nuc_mole(i, frac=q_nuc_occ[idx]))
+                idx += 1

pyscf/neo/hf.py
@@ -47,11 +47,62 @@ def get_hcore_nuc(mol, dm_elec, dm_nuc, mol_elec=None, mol_nuc=None):
     return h
 
 def get_occ_nuc(mf):
+    '''Label the occupation for each orbital of the quantum nucleus'''
     def get_occ(mo_energy=mf.mo_energy, mo_coeff=mf.mo_coeff):
-        '''Label the occupation for each orbital of the quantum nucleus'''
         e_idx = numpy.argsort(mo_energy)
         mo_occ = numpy.zeros(mo_energy.size)
-        mo_occ[e_idx[mf.occ_state]] = 1
+        mo_occ[e_idx[mf.occ_state]] = mf.mol.nnuc # 1 or fractional
+        return mo_occ
+    return get_occ
+
+def get_occ_elec(mf):
+    '''To support fractional occupations'''
+    def get_occ(mo_energy=mf.mo_energy, mo_coeff=mf.mo_coeff):
+        if mo_energy is None: mo_energy = mf.mo_energy
+        e_idx_a = numpy.argsort(mo_energy[0])
+        e_idx_b = numpy.argsort(mo_energy[1])
+        e_sort_a = mo_energy[0][e_idx_a]
+        e_sort_b = mo_energy[1][e_idx_b]
+        nmo = mo_energy[0].size
+        n_a, n_b = mf.nelec
+        mo_occ = numpy.zeros_like(mo_energy)
+        # change the homo occupation to fractional
+        if n_a > n_b or n_b <= 0:
+            mo_occ[0,e_idx_a[:n_a - 1]] = 1
+            mo_occ[0,e_idx_a[n_a - 1]] = mf.mol.nhomo
+            mo_occ[1,e_idx_b[:n_b]] = 1
+        else:
+            mo_occ[1,e_idx_b[:n_b - 1]] = 1
+            mo_occ[1,e_idx_b[n_b - 1]] = mf.mol.nhomo
+            mo_occ[0,e_idx_a[:n_a]] = 1
+        if mf.verbose >= logger.INFO and n_a < nmo and n_b > 0 and n_b < nmo:
+            if e_sort_a[n_a-1]+1e-3 > e_sort_a[n_a]:
+                logger.warn(mf, 'alpha nocc = %d  HOMO %.15g >= LUMO %.15g',
+                            n_a, e_sort_a[n_a-1], e_sort_a[n_a])
+            else:
+                logger.info(mf, '  alpha nocc = %d  HOMO = %.15g  LUMO = %.15g',
+                            n_a, e_sort_a[n_a-1], e_sort_a[n_a])
+
+            if e_sort_b[n_b-1]+1e-3 > e_sort_b[n_b]:
+                logger.warn(mf, 'beta  nocc = %d  HOMO %.15g >= LUMO %.15g',
+                            n_b, e_sort_b[n_b-1], e_sort_b[n_b])
+            else:
+                logger.info(mf, '  beta  nocc = %d  HOMO = %.15g  LUMO = %.15g',
+                            n_b, e_sort_b[n_b-1], e_sort_b[n_b])
+
+            if e_sort_a[n_a-1]+1e-3 > e_sort_b[n_b]:
+                logger.warn(mf, 'system HOMO %.15g >= system LUMO %.15g',
+                            e_sort_b[n_a-1], e_sort_b[n_b])
+
+            numpy.set_printoptions(threshold=nmo)
+            logger.debug(mf, '  alpha mo_energy =\n%s', mo_energy[0])
+            logger.debug(mf, '  beta  mo_energy =\n%s', mo_energy[1])
+            numpy.set_printoptions(threshold=1000)
+
+        if mo_coeff is not None and mf.verbose >= logger.DEBUG:
+            ss, s = mf.spin_square((mo_coeff[0][:,mo_occ[0]>0],
+                                    mo_coeff[1][:,mo_occ[1]>0]), mf.get_ovlp())
+            logger.debug(mf, 'multiplicity <S^2> = %.8g  2S+1 = %.8g', ss, s)
         return mo_occ
     return get_occ
 
@@ -467,6 +518,8 @@ class HF(scf.hf.SCF):
 
     def __init__(self, mol, unrestricted=False):
         scf.hf.SCF.__init__(self, mol)
+        if mol.elec.nhomo is not None:
+            unrestricted = True
         self.unrestricted = unrestricted
         self.mf_elec = None
         # dm_elec will be the total density after SCF, but can be spin
@@ -490,6 +543,8 @@ class HF(scf.hf.SCF):
         self.mf_elec.get_hcore = self.get_hcore_elec
         self.mf_elec.get_h_ep = self.get_h_ep_elec
         self.mf_elec.super_mf = self
+        if mol.elec.nhomo is not None:
+            self.mf_elec.get_occ = self.get_occ_elec(self.mf_elec)
 
         # nuclear part
         for i in range(mol.nuc_num):
@@ -523,6 +578,9 @@ class HF(scf.hf.SCF):
 
     def get_occ_nuc(self, mf):
         return get_occ_nuc(mf)
+
+    def get_occ_elec(self, mf):
+        return get_occ_elec(mf)
 
     def get_hcore_nuc(self, mol):
         return get_hcore_nuc(mol, self.dm_elec, self.dm_nuc, self.mol.elec, self.mol.nuc)

pyscf/neo/ks.py
@@ -67,6 +67,8 @@ class KS(HF):
     def __init__(self, mol, unrestricted=False, epc=None):
         HF.__init__(self, mol)
 
+        if mol.elec.nhomo is not None:
+            unrestricted = True
         self.unrestricted = unrestricted
         self.epc = epc # electron-proton correlation: '17-1' or '17-2' can be used
 
@@ -83,6 +85,8 @@ class KS(HF):
         else:
             self.mf_elec.get_h_ep = self.get_h_ep_elec
         self.mf_elec.super_mf = self
+        if mol.elec.nhomo is not None:
+            self.mf_elec.get_occ = self.get_occ_elec(self.mf_elec)
 
         # build grids (Note: high-density grids are needed since nuclei is more localized than electrons)
         self.mf_elec.grids.build(with_non0tab=False)

commit 1c4a9d7902e574e6a6f9d2e3393fc9083d80b3bc

NEO: fix the bug that fractional code ruins integer calculations
The bug was introduced in 6fe9523b278ab82f97d4032ea71fa887b29255ba

pyscf/neo/mole.py
@@ -76,7 +76,10 @@ class Mole(gto.mole.Mole):
         nuc.nelectron = 2
 
         # fractional
-        nuc.nnuc = frac
+        if frac is not None:
+            nuc.nnuc = frac
+        else:
+            nuc.nnuc = 1
 
         return nuc
 