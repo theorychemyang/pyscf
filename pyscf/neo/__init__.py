3cb56debbe801aa29e3575c810db3d8e1c251d14
Yuzhe Zhang <yuzhezhang98@gmail.com>
2022-12-16 16:57:44 -0600

    ase: add Pyscf_DFT_VV10 and Pyscf_NEO_VV10 calculators

--- pyscf/neo/__init__.py
+++ pyscf/neo/__init__.py
@@ -7,6 +7,6 @@ from pyscf.neo.cdft import CDFT
 from pyscf.neo.grad import Gradients
 from pyscf.neo.cphf import CPHF
 from pyscf.neo.hessian import Hessian
-from pyscf.neo.ase import Pyscf_NEO, Pyscf_DFT
+from pyscf.neo.ase import Pyscf_NEO, Pyscf_DFT, Pyscf_DFT_VV10, Pyscf_NEO_VV10
 from pyscf.neo.mp2 import MP2
 #import pyscf.neo.pbc
--- pyscf/neo/ase.py
+++ pyscf/neo/ase.py
@@ -173,3 +173,142 @@ class Pyscf_DFT(Calculator):
             g.grid_response = self.parameters.grid_response
         self.results['forces'] = -g.grad()*Hartree/Bohr
         self.results['dipole'] = dip_moment(mol, mf.make_rdm1())
+
+
+class Pyscf_DFT_VV10(Calculator):
+
+    implemented_properties = ['energy', 'forces', 'dipole']
+    default_parameters = {'basis': 'ccpvdz',
+                          'charge': 0,
+                          'spin': 0,
+                          'xc': 'b3lyp',
+                          'atom_grid' : None, # recommend (99,590) or even (99,974)
+                          'grid_response' : None, # for meta-GGA, must be True!!!
+                          'init_guess' : None, # 'huckel' for unrestricted might be good
+                          'conv_tol' : None, # 1e-12 for tight convergence
+                          'conv_tol_grad' : None # 1e-8 for tight convergence
+                         }
+
+
+    def __init__(self, **kwargs):
+        Calculator.__init__(self, **kwargs)
+
+    def calculate(self, atoms=None, properties=['energy', 'forces', 'dipole'],
+                  system_changes=['positions', 'numbers', 'cell', 'pbc', 'charges', 'magmoms']):
+        Calculator.calculate(self, atoms, properties, system_changes)
+        mol = gto.Mole()
+        atoms = self.atoms.get_chemical_symbols()
+        positions = self.atoms.get_positions()
+        atom_pyscf = []
+        for i in range(len(atoms)):
+            if atoms[i] == 'D':
+                atom_pyscf.append(['H+', tuple(positions[i])])
+            else:
+                atom_pyscf.append(['%s' %(atoms[i]), tuple(positions[i])])
+        mol.build(atom = atom_pyscf, basis = self.parameters.basis,
+                  charge = self.parameters.charge, spin = self.parameters.spin)
+        if self.parameters.spin != 0:
+            mf = dft.UKS(mol)
+        else:
+            mf = dft.RKS(mol)
+        mf.xc = self.parameters.xc
+        mf.nlc = 'VV10'
+        if self.parameters.atom_grid is not None:
+            mf.grids.atom_grid = self.parameters.atom_grid
+        mf.grids.prune=None
+        mf.nlcgrids.atom_grid=(50,194)
+        mf.nlcgrids.prune=dft.gen_grid.sg1_prune
+        if self.parameters.init_guess is not None:
+            mf.init_guess = self.parameters.init_guess
+        if self.parameters.conv_tol is not None:
+            mf.conv_tol = self.parameters.conv_tol
+        if self.parameters.conv_tol_grad is not None:
+            mf.conv_tol_grad = self.parameters.conv_tol_grad
+        # check stability for UKS
+        mf.scf()
+        if self.parameters.spin !=0:
+            mf = stable_opt_internal(mf)
+        self.results['energy'] = mf.e_tot*Hartree
+        g = mf.Gradients()
+        if self.parameters.grid_response is not None:
+            g.grid_response = self.parameters.grid_response
+        self.results['forces'] = -g.grad()*Hartree/Bohr
+        self.results['dipole'] = dip_moment(mol, mf.make_rdm1())
+
+
+class Pyscf_NEO_VV10(Calculator):
+
+    implemented_properties = ['energy', 'forces', 'dipole']
+    default_parameters = {'basis': 'ccpvdz',
+                          'charge': 0,
+                          'spin': 0,
+                          'xc': 'b3lyp',
+                          'quantum_nuc': ['H'],
+                          'atom_grid' : None, # recommend (99,590) or even (99,974)
+                          'grid_response' : None, # for meta-GGA, must be True!!!
+                          'init_guess' : None, # 'huckel' for unrestricted might be good
+                          'conv_tol' : None, # 1e-12 for tight convergence
+                          'conv_tol_grad' : None # 1e-8 for tight convergence
+                         }
+
+
+    def __init__(self, **kwargs):
+        Calculator.__init__(self, **kwargs)
+
+    def calculate(self, atoms=None, properties=['energy', 'forces', 'dipole'],
+                  system_changes=['positions', 'numbers', 'cell', 'pbc', 'charges', 'magmoms']):
+        Calculator.calculate(self, atoms, properties, system_changes)
+        mol = neo.Mole()
+        atoms = self.atoms.get_chemical_symbols()
+        ase_masses = self.atoms.get_masses()
+        positions = self.atoms.get_positions()
+        atom_pyscf = []
+        for i in range(len(atoms)):
+            if atoms[i] == 'Mu':
+                atom_pyscf.append(['H@0', tuple(positions[i])])
+            elif atoms[i] == 'D':
+                atom_pyscf.append(['H+%i' %i, tuple(positions[i])])
+            elif atoms[i] == 'H' and abs(ase_masses[i]-2.014) < 0.01:
+                # this is for person who does not want to modify ase
+                # by changing the mass array, pyscf still accepts H as D
+                atom_pyscf.append(['H+%i' %i, tuple(positions[i])])
+            else:
+                atom_pyscf.append(['%s%i' %(atoms[i],i), tuple(positions[i])])
+        mol.build(quantum_nuc = self.parameters.quantum_nuc,
+                  atom = atom_pyscf,
+                  basis = self.parameters.basis,
+                  charge = self.parameters.charge, spin = self.parameters.spin)
+        if self.parameters.spin == 0:
+            mf = neo.CDFT(mol)
+        else:
+            mf = neo.CDFT(mol, unrestricted = True)
+        mf.mf_elec.xc = self.parameters.xc
+        mf.mf_elec.nlc = 'VV10'
+        if self.parameters.atom_grid is not None:
+            mf.mf_elec.grids.atom_grid = self.parameters.atom_grid
+        mf.mf_elec.grids.prune = None
+        mf.mf_elec.nlcgrids.atom_grid=(50,194)
+        mf.mf_elec.nlcgrids.prune=dft.gen_grid.sg1_prune
+        if self.parameters.init_guess is not None:
+            mf.init_guess = self.parameters.init_guess
+        if self.parameters.conv_tol is not None:
+            mf.conv_tol = self.parameters.conv_tol
+        if self.parameters.conv_tol_grad is not None:
+            mf.conv_tol_grad = self.parameters.conv_tol_grad
+        # check stability for UKS
+        mf.scf()
+        if self.parameters.spin !=0:
+            mf = stable_opt_internal(mf)
+        self.results['energy'] = mf.e_tot*Hartree
+        g = mf.Gradients()
+        if self.parameters.grid_response is not None:
+            g.grid_response = self.parameters.grid_response
+        self.results['forces'] = -g.grad()*Hartree/Bohr
+
+        dip_elec = dip_moment(mol.elec, mf.mf_elec.make_rdm1()) # dipole of electrons and classical nuclei
+        dip_nuc = 0
+        for i in range(len(mf.mf_nuc)):
+            ia = mf.mf_nuc[i].mol.atom_index
+            dip_nuc += mol.atom_charge(ia) * mf.mf_nuc[i].nuclei_expect_position * nist.AU2DEBYE
+
+        self.results['dipole'] = dip_elec + dip_nuc
