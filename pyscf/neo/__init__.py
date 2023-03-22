2e3023671b6c2d63efdfe9f96b190e184784a16a
zc62 <17048823+zc62@users.noreply.github.com>
2023-03-22 16:38:20 -0500

    NEO: allow not having ASE

--- pyscf/neo/__init__.py
+++ pyscf/neo/__init__.py
@@ -6,5 +6,8 @@ from pyscf.neo.ks import KS
 from pyscf.neo.cdft import CDFT
 from pyscf.neo.grad import Gradients
 from pyscf.neo.hessian import Hessian
-from pyscf.neo.ase import Pyscf_NEO, Pyscf_DFT
+try:
+    from pyscf.neo.ase import Pyscf_NEO, Pyscf_DFT
+except ImportError:
+    pass
 from pyscf.neo.mp2 import MP2
