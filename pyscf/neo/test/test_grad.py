46916ce26421dbfa10c61052a6fccb0b6846ab7b
Yuzhe Zhang <yuzhezhang98@gmail.com>
2023-05-17 09:56:18 -0500

    NEO/epc: implement analytic gradient for epc
    There is still a systematic ~1e-5 au difference between analytic and
    8 point finite difference gradient.

--- pyscf/neo/test/test_grad.py
+++ pyscf/neo/test/test_grad.py
@@ -34,6 +34,27 @@ class KnownValues(unittest.TestCase):
 
         self.assertAlmostEqual(de[0,2], (e2-e1)/0.002*lib.param.BOHR, 5)
 
+    def test_epc_grad(self):
+        mol = neo.M(atom='''H 0 0 0; F 0 0 0.94''', basis='def2-tzvppd',
+                    quantum_nuc=[0])
+        mf = neo.CDFT(mol, epc='17-2')
+        mf.mf_elec.grids.atom_grid = (99,590)
+        mf.run()
+        de = mf.nuc_grad_method().kernel()
+        mfs = mf.as_scanner()
+        e1 = mfs('H 0 0 -0.0004; F 0 0 0.94')
+        e2 = mfs('H 0 0 -0.0003; F 0 0 0.94')
+        e3 = mfs('H 0 0 -0.0002; F 0 0 0.94')
+        e4 = mfs('H 0 0 -0.0001; F 0 0 0.94')
+        e5 = mfs('H 0 0 0.0001; F 0 0 0.94')
+        e6 = mfs('H 0 0 0.0002; F 0 0 0.94')
+        e7 = mfs('H 0 0 0.0003; F 0 0 0.94')
+        e8 = mfs('H 0 0 0.0004; F 0 0 0.94')
+
+        fd = 1/280 * e1 + -4/105 * e2 + 1/5 * e3 + -4/5 * e4 \
+             + 4/5 * e5 + -1/5 * e6 + 4/105 * e7 - 1/280 * e8
+        self.assertAlmostEqual(de[0,2], fd/0.0001*lib.param.BOHR, 5)
+
 
 if __name__ == "__main__":
     print("Full Tests for neo.ks")
