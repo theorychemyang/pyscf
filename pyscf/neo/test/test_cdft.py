theorychemyang/pyscf

commit 2002e3ad53e6ee72174b567027774d800179519d

NEO: use common isotope mass for heavy nuclei

pyscf/neo/mole.py
@@ -237,6 +237,7 @@ class Mole(gto.mole.Mole):
             self.nuc_num = len([i for i in self.quantum_nuc if i])
 
         if self.mass is None:
+            mass_list_not_avg = self.atom_mass_list(isotope_avg=False)
             self.mass = self.atom_mass_list(isotope_avg=True)
             for i in range(self.natm):
                 if 'H+' in self.atom_symbol(i): # Deuterium (from Wikipedia)
@@ -253,6 +254,9 @@ class Mole(gto.mole.Mole):
                        'H#' not in self.atom_symbol(i) and \
                         self.atom_pure_symbol(i) == 'H': # Hydrogen (from Wikipedia)
                         self.mass[i] = 1.007825
+                    else:
+                        # if quantum, use the most common isotope mass
+                        self.mass[i] = mass_list_not_avg[i]
                     # subtract electron mass to get nuclear mass
                     # the biggest error is from isotope_avg, though
                     self.mass[i] -= self.atom_charge(i) * nist.E_MASS / nist.ATOMIC_MASS

pyscf/gto/mole.py
@@ -1931,7 +1931,9 @@ def atom_mass_list(mol, isotope_avg=False):
     if isotope_avg:
         mass_table = elements.MASSES
     else:
-        mass_table = elements.ISOTOPE_MAIN
+        # changed this to make full quantum CNEO work better
+        #mass_table = elements.ISOTOPE_MAIN
+        mass_table = elements.COMMON_ISOTOPE_MASSES
 
     nucprop = mol.nucprop
     if nucprop:

pyscf/neo/test/test_hess.py
@@ -63,9 +63,9 @@ class KnownValues(unittest.TestCase):
         h = hess.kernel()
         results = hess.harmonic_analysis(mol, h)
 
-        self.assertAlmostEqual(results['freq_wavenumber'][-1], 3720.750, 2)
-        self.assertAlmostEqual(results['freq_wavenumber'][-2], 3619.598, 2)
-        self.assertAlmostEqual(results['freq_wavenumber'][-3], 1572.554, 2)
+        self.assertAlmostEqual(results['freq_wavenumber'][-1], 3720.785, 2)
+        self.assertAlmostEqual(results['freq_wavenumber'][-2], 3619.616, 2)
+        self.assertAlmostEqual(results['freq_wavenumber'][-3], 1572.570, 2)
 
     def test_hess_HF_uks(self):
         mol = neo.Mole()
@@ -125,9 +125,9 @@ class KnownValues(unittest.TestCase):
         h = hess.kernel()
         results = hess.harmonic_analysis(mol, h)
 
-        self.assertAlmostEqual(results['freq_wavenumber'][-1], 3720.750, 2)
-        self.assertAlmostEqual(results['freq_wavenumber'][-2], 3619.598, 2)
-        self.assertAlmostEqual(results['freq_wavenumber'][-3], 1572.554, 2)
+        self.assertAlmostEqual(results['freq_wavenumber'][-1], 3720.785, 2)
+        self.assertAlmostEqual(results['freq_wavenumber'][-2], 3619.616, 2)
+        self.assertAlmostEqual(results['freq_wavenumber'][-3], 1572.570, 2)
 
     def test_hess_CH3_full_q_uks(self):
         mol = neo.Mole()
@@ -144,9 +144,9 @@ class KnownValues(unittest.TestCase):
         h = hess.kernel()
         results = hess.harmonic_analysis(mol, h)
 
-        self.assertAlmostEqual(results['freq_wavenumber'][-1], 3500.798, 2)
-        self.assertAlmostEqual(results['freq_wavenumber'][-3], 3295.651, 2)
-        self.assertAlmostEqual(results['freq_wavenumber'][-4], 1242.949, 2)
+        self.assertAlmostEqual(results['freq_wavenumber'][-1], 3500.978, 2)
+        self.assertAlmostEqual(results['freq_wavenumber'][-3], 3295.659, 2)
+        self.assertAlmostEqual(results['freq_wavenumber'][-4], 1242.998, 2)
 
 if __name__ == "__main__":
     print("Full Tests for neo.hess")

pyscf/neo/test/test_hf.py
@@ -17,14 +17,14 @@ class KnownValues(unittest.TestCase):
         mol.build(atom='''H 0 0 0; C 0 0 1.064; N 0 0 2.220''', basis='ccpvdz', quantum_nuc=[0,1])
         mf = neo.HF(mol)
         energy = mf.scf()
-        self.assertAlmostEqual(energy, -92.3015123882943, 8)
+        self.assertAlmostEqual(energy, -92.30126987320989, 8)
 
     def test_scf3(self):
         mol = neo.Mole()
         mol.build(atom='''H 0 0 0; C 0 0 1.064; N 0 0 2.220''', basis='ccpvdz', quantum_nuc=[0,1,2])
         mf = neo.HF(mol)
         energy = mf.scf()
-        self.assertAlmostEqual(energy, -91.6088626429504, 8)
+        self.assertAlmostEqual(energy, -91.60852522077765, 8)
 
 
 if __name__ == "__main__":

commit 8236b921f30eeb29fdcaace73d91f629f689e1f1

Fix a critical bug for H isotope calculations; add D test
The bug was introduced in 2002e3ad53e6ee72174b567027774d800179519d. The
later if statement will be executed even if the D mass is already set,
then it is reverted to H isotope mass. After that commit and before this
fix commit, all D/Mu/HeMu isotope calculations will just be pure H
calculations.

Now add an isotope test calculation to test_cdft.py to make sure similar
errors can be detected.

pyscf/neo/mole.py
@@ -237,6 +237,10 @@ class Mole(gto.mole.Mole):
             self.nuc_num = len([i for i in self.quantum_nuc if i])
 
         if self.mass is None:
+            # Use the most common isotope mass, not isotope_avg mass
+            # NOTE: the definition of gto.mole.atom_mass_list is modified.
+            # originally it returns elements.ISOTOPE_MAIN, now I change it
+            # to elements.COMMON_ISOTOPE_MASSES, which I think makes more sense
             mass_list_not_avg = self.atom_mass_list(isotope_avg=False)
             self.mass = self.atom_mass_list(isotope_avg=True)
             for i in range(self.natm):
@@ -247,16 +251,10 @@ class Mole(gto.mole.Mole):
                 elif 'H#' in self.atom_symbol(i): # Muonic Helium without electron = He4 nucleus + Muon
                     # He4 atom mass from Wikipedia
                     self.mass[i] = 4.002603254 - nist.E_MASS / nist.ATOMIC_MASS + 0.1134289259
+                else:
+                    # use the most common isotope mass. For H, it is 1.007825
+                    self.mass[i] = mass_list_not_avg[i]
                 if self.quantum_nuc[i]:
-                    # Use pure H1 isotope mass only when it is quantum, otherwise isotope average
-                    if 'H+' not in self.atom_symbol(i) and \
-                       'H*' not in self.atom_symbol(i) and \
-                       'H#' not in self.atom_symbol(i) and \
-                        self.atom_pure_symbol(i) == 'H': # Hydrogen (from Wikipedia)
-                        self.mass[i] = 1.007825
-                    else:
-                        # if quantum, use the most common isotope mass
-                        self.mass[i] = mass_list_not_avg[i]
                     # subtract electron mass to get nuclear mass
                     # the biggest error is from isotope_avg, though
                     self.mass[i] -= self.atom_charge(i) * nist.E_MASS / nist.ATOMIC_MASS

pyscf/neo/test/test_cdft.py
@@ -35,6 +35,11 @@ class KnownValues(unittest.TestCase):
         mf = neo.CDFT(mol, epc='18-2')
         self.assertAlmostEqual(mf.scf(), -93.36401432623929, 6)
 
+    def test_isotope(self):
+        mol_D_atom = neo.M(atom='H+ 0 0 0', spin=1)
+        mf = neo.CDFT(mol_D_atom)
+        self.assertAlmostEqual(mf.scf(), -0.432707430519338, 6)
+
 
 if __name__ == "__main__":
     print("Full Tests for neo.cdft")