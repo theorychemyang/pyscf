theorychemyang/pyscf

commit 98273223a5edaefba84e74fd16b437d5039db1f8

NEO: show time and memory usage for incore ERIs
This also shows basic pyscf scf e-e ERI time/memory usages. If memory is
not enough, warnings will tell how much memory is needed.

Can force scf use direct vee via the `direct_vee` keyword.

pyscf/neo/hf.py
@@ -71,9 +71,11 @@ def _is_mem_enough(nao1, nao2, max_memory):
     return nao1**2*nao2**2*2/1e6+lib.current_memory()[0] < max_memory*.95
 
 def _build_eri_ne(mol_elec, mol_nuc):
+    if mol_elec.super_mol.verbose >= logger.DEBUG:
+        cput0 = (logger.process_clock(), logger.perf_counter())
     eri_ne = None
-    if mol_elec.super_mol.incore_anyway or \
-        _is_mem_enough(mol_elec.nao_nr(), mol_nuc.nao_nr(), mol_elec.super_mol.max_memory):
+    if not mol_elec.super_mol.direct_vee and (mol_elec.super_mol.incore_anyway or
+        _is_mem_enough(mol_elec.nao_nr(), mol_nuc.nao_nr(), mol_elec.super_mol.max_memory)):
         atm, bas, env = gto.conc_env(mol_nuc._atm, mol_nuc._bas, mol_nuc._env,
                                      mol_elec._atm, mol_elec._bas, mol_elec._env)
         intor_name = 'int2e_sph'
@@ -87,13 +89,19 @@ def _build_eri_ne(mol_elec, mol_nuc):
                                               mol_nuc._bas.shape[0],
                                               mol_nuc._bas.shape[0] + mol_elec._bas.shape[0]),
                                   aosym='s4')
+    if mol_elec.super_mol.verbose >= logger.DEBUG and eri_ne is not None:
+        logger.timer(mol_elec.super_mol,
+                     f'Incore ERI between electron and {mol_nuc.index}-th nucleus', *cput0)
+        logger.debug(mol_elec.super_mol, f'Memory usage: {eri_ne.nbytes/1024**2:.3f} MB')
     return eri_ne
 
 def _build_eri_nn(mol_nuc1, mol_nuc2):
     '''idx mole_nuc2 > mole_nuc1'''
+    if mol_nuc1.super_mol.verbose >= logger.DEBUG:
+        cput0 = (logger.process_clock(), logger.perf_counter())
     eri_nn = None
-    if mol_nuc1.super_mol.incore_anyway or \
-        _is_mem_enough(mol_nuc1.nao_nr(), mol_nuc2.nao_nr(), mol_nuc1.super_mol.max_memory):
+    if not mol_nuc1.super_mol.direct_vee and (mol_nuc1.super_mol.incore_anyway or
+        _is_mem_enough(mol_nuc1.nao_nr(), mol_nuc2.nao_nr(), mol_nuc1.super_mol.max_memory)):
         atm, bas, env = gto.conc_env(mol_nuc1._atm, mol_nuc1._bas, mol_nuc1._env,
                                      mol_nuc2._atm, mol_nuc2._bas, mol_nuc2._env)
         intor_name = 'int2e_sph'
@@ -107,6 +115,11 @@ def _build_eri_nn(mol_nuc1, mol_nuc2):
                                               mol_nuc1._bas.shape[0],
                                               mol_nuc1._bas.shape[0] + mol_nuc2._bas.shape[0]),
                                   aosym='s4')
+    if mol_nuc1.super_mol.verbose >= logger.DEBUG and eri_nn is not None:
+        logger.timer(mol_nuc1.super_mol,
+                     f'Incore ERI between {mol_nuc1.index}-th nucleus '
+                     + f'and {mol_nuc2.index}-th nucleus', *cput0)
+        logger.debug(mol_nuc1.super_mol, f'Memory usage: {eri_nn.nbytes/1024**2:.3f} MB')
     return eri_nn
 
 def get_j_e_dm_n(idx_nuc, dm_n, mol_elec=None, mol_nuc=None, eri_ne=None):
@@ -119,7 +132,10 @@ def get_j_e_dm_n(idx_nuc, dm_n, mol_elec=None, mol_nuc=None, eri_ne=None):
         if eri_ne[idx_nuc] is not None:
             return -charge * dot_eri_dm(eri_ne[idx_nuc], dm_n, nao_v=mol_elec.nao_nr(),
                                         eri_dot_dm=False)
-    warnings.warn("Direct Vee is used, might be slow. Check PYSCF_MAX_MEMORY?")
+    if not mol_elec.super_mol.direct_vee:
+        warnings.warn('Direct Vee is used for e-n ERIs, might be slow. '
+                      +f'PYSCF_MAX_MEMORY is set to {mol_elec.super_mol.max_memory} MB, '
+                      +f'required memory: {mol_elec.nao_nr()**2*mol_nuc.nao_nr()**2*2/1e6=:.2f} MB')
     return -charge * scf.jk.get_jk((mol_elec, mol_elec, mol_nuc, mol_nuc),
                                    dm_n, scripts='ijkl,lk->ij',
                                    intor='int2e', aosym='s4')
@@ -134,7 +150,10 @@ def get_j_n_dm_e(idx_nuc, dm_e, mol_elec=None, mol_nuc=None, eri_ne=None):
         if eri_ne[idx_nuc] is not None:
             return -charge * dot_eri_dm(eri_ne[idx_nuc], dm_e, nao_v=mol_nuc.nao_nr(),
                                         eri_dot_dm=True)
-    warnings.warn("Direct Vee is used, might be slow. Check PYSCF_MAX_MEMORY?")
+    if not mol_elec.super_mol.direct_vee:
+        warnings.warn('Direct Vee is used for e-n ERIs, might be slow. '
+                      +f'PYSCF_MAX_MEMORY is set to {mol_elec.super_mol.max_memory} MB, '
+                      +f'required memory: {mol_elec.nao_nr()**2*mol_nuc.nao_nr()**2*2/1e6=:.2f} MB')
     return -charge * scf.jk.get_jk((mol_nuc, mol_nuc, mol_elec, mol_elec),
                                    dm_e, scripts='ijkl,lk->ij',
                                    intor='int2e', aosym='s4')
@@ -161,7 +180,10 @@ def get_j_nn(idx1, idx2, dm_n2, mol_nuc1=None, mol_nuc2=None, eri_nn=None):
                                            eri_dot_dm=False)
         elif idx1 == idx2:
             return 0.0
-    warnings.warn("Direct Vee is used, might be slow. Check PYSCF_MAX_MEMORY?")
+    if not mol_nuc1.super_mol.direct_vee:
+        warnings.warn('Direct Vee is used for n-n ERIs, might be slow. '
+                      +f'PYSCF_MAX_MEMORY is set to {mol_nuc1.super_mol.max_memory} MB, '
+                      +f'required memory: {mol_nuc1.nao_nr()**2*mol_nuc2.nao_nr()**2*2/1e6=:.2f} MB')
     return charge * scf.jk.get_jk((mol_nuc1, mol_nuc1, mol_nuc2, mol_nuc2),
                                   dm_n2, scripts='ijkl,lk->ij',
                                   intor='int2e', aosym='s4')
@@ -215,16 +237,7 @@ def get_hcore_nuc(mol, mf_nuc, dm_elec, dm_nuc, mol_elec=None,
         if super_mol.mm_mol is not None:
             mf_nuc.hcore_static += hcore_nuc_qmmm(super_mol.mm_mol, mol, charge)
     h = 0
-    # find the index of mol
-    # TODO: store this information
-    i = 0
-    found = False
-    for i in range(len(mol_nuc)):
-        if ia == mol_nuc[i].atom_index:
-            found = True
-            break
-    if not found:
-        raise RuntimeError('Failed to find the index of the quantum nucleus')
+    i = mol.index
     # Coulomb interaction between the quantum nucleus and electrons
     ne_U = 0.0
     if dm_elec.ndim > 2:

pyscf/scf/hf.py
@@ -26,6 +26,7 @@ import tempfile
 from functools import reduce
 import numpy
 import scipy.linalg
+import warnings
 import h5py
 from pyscf import gto
 from pyscf import lib
@@ -2057,12 +2058,21 @@ class RHF(SCF):
         # Note the incore version, which initializes an _eri array in memory.
         if mol is None: mol = self.mol
         if dm is None: dm = self.make_rdm1()
-        if (not omega and
+        if (not omega and not mol.direct_vee and
             (self._eri is not None or mol.incore_anyway or self._is_mem_enough())):
             if self._eri is None:
+                if mol.verbose >= logger.DEBUG:
+                    cput0 = (logger.process_clock(), logger.perf_counter())
                 self._eri = mol.intor('int2e', aosym='s8')
+                if mol.verbose >= logger.DEBUG:
+                    logger.timer(mol, 'Incore e-e ERI', *cput0)
+                    logger.debug(mol, f'Memory usage: {self._eri.nbytes/1024**2:.3f} MB')
             vj, vk = dot_eri_dm(self._eri, dm, hermi, with_j, with_k)
         else:
+            if not mol.direct_vee:
+                warnings.warn('Direct Vee is used for e-e ERIs, might be slow. '
+                              +f'PYSCF_MAX_MEMORY is set to {mol.max_memory} MB, '
+                              +f'required memory: {mol.nao_nr()**4/1e6=:.2f} MB')
             vj, vk = SCF.get_jk(self, mol, dm, hermi, with_j, with_k, omega)
         return vj, vk
 

pyscf/gto/mole.py
@@ -2237,6 +2237,8 @@ class Mole(lib.StreamObject):
     # Whether to hold everything in memory
     incore_anyway = getattr(__config__, 'INCORE_ANYWAY', False)
 
+    direct_vee = getattr(__config__, 'DIRECT_VEE', False)
+
     # Using cartesian GTO (6d,10f,15g)
     cart = getattr(__config__, 'gto_mole_Mole_cart', False)
 
@@ -2296,7 +2298,7 @@ class Mole(lib.StreamObject):
         # Some methods modify ._env. These method are executed in the context
         # _TemporaryMoleContext which is protected by the _ctx_lock.
         self._ctx_lock = None
-        keys = set(('verbose', 'unit', 'cart', 'incore_anyway'))
+        keys = set(('verbose', 'unit', 'cart', 'incore_anyway', 'direct_vee'))
         self._keys = set(self.__dict__.keys()).union(keys)
         self.__dict__.update(kwargs)
 

pyscf/neo/mole.py
@@ -40,7 +40,7 @@ def copy(mol):
 
     return newmol
 
-def build_nuc_mole(mol, atom_index, nuc_basis, frac=None):
+def build_nuc_mole(mol, index, atom_index, nuc_basis, frac=None):
     '''
     Return a Mole object for specified quantum nuclei.
 
@@ -54,6 +54,7 @@ def build_nuc_mole(mol, atom_index, nuc_basis, frac=None):
     nuc = gto.Mole() # a Mole object for quantum nuclei
     nuc.super_mol = mol
     nuc.atom_index = atom_index
+    nuc.index = index
 
     # e.g., PB4-D, PB4D and PB4_D will all be converted to pb4d
     nuc_basis = nuc_basis.replace('-', '').replace('_', '').lower()
@@ -158,7 +159,7 @@ def build_nuc_mole(mol, atom_index, nuc_basis, frac=None):
     else:
         nuc.nnuc = 1
 
-    nuc._keys.update(['super_mol', 'atom_index', 'nnuc'])
+    nuc._keys.update(['super_mol', 'index', 'atom_index', 'nnuc'])
 
     return nuc
 
@@ -309,7 +310,7 @@ class Mole(gto.mole.Mole):
             self.nuc = []
             for i in range(self.natm):
                 if self.quantum_nuc[i]:
-                    self.nuc.append(build_nuc_mole(self, i,
+                    self.nuc.append(build_nuc_mole(self, idx, i,
                                                    nuc_basis=self.nuc_basis,
                                                    frac=q_nuc_occ[idx]))
                     idx += 1

commit b570bb71e02c96ae13f120422984e7d51309672c

NEO: fix bug introduced by 98273223a5edaefba84e74fd16b437d5039db1f8
In that commit, during the initial guess procedure, only the first
quantum nuc-electron integrals are calculated, and all others use the
incorrect integral. Fix the bug by correctly assign the index for
temporary nuc moles.

pyscf/neo/hf.py
@@ -652,6 +652,7 @@ def init_guess_nuc_by_calculation(mf_nuc, mol):
                       ecp=mol.ecp, charge=mol.charge, spin=mol.spin,
                       symmetry=mol.symmetry, symmetry_subgroup=mol.symmetry_subgroup,
                       cart=mol.cart, magmom=mol.magmom)
+        mol_tmp.nuc[0].index = mol.nuc[i].index
         dm_nuc[i] = get_init_guess_nuc(mf_nuc[i], mol_tmp.nuc[0])
         mf_nuc[i].hcore_static = None # clear the true hcore cache
     return dm_nuc