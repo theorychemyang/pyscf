ca70700efb4ba0eb1358deb8fff353310abac2c2
Haoran Chen <youskoraginsl@hotmail.com>
2025-11-07 13:47:37 -0600

    Polarizability class for (C)NEO; add NEO dipole
    Extend e_field.py polarizability to a class Polarizability for both NEO
    and CNEO. Add dipole moment to unconstrained NEO.

--- pyscf/neo/hf.py
+++ pyscf/neo/hf.py
@@ -494,12 +494,20 @@ class ComponentSCF(Component):
     def dip_moment(self, mol=None, dm=None, unit='Debye', origin=None, verbose=logger.NOTE,
                    **kwargs):
         if self.is_nucleus:
-            return None
+            if origin is None:
+                origin = numpy.zeros(3)
+            else:
+                origin = numpy.asarray(origin, dtype=numpy.float64)
+            assert origin.shape == (3,)
+            dip = -self.charge * (lib.einsum('xij,ji->x', self.mol.intor_symmetric('int1e_r', comp=3), dm) - origin)
+            if unit.upper() == 'DEBYE':
+                dip *= nist.AU2DEBYE
         # Temporarily modify charge to supress warning about nonzero charge for a neutral molecule
-        charge = self.mol.charge
-        self.mol.charge = self.mol.super_mol.charge
-        dip = super().dip_moment(mol, dm, unit, origin=origin, verbose=verbose, **kwargs)
-        self.mol.charge = charge
+        else:
+            charge = self.mol.charge
+            self.mol.charge = self.mol.super_mol.charge
+            dip = super().dip_moment(mol, dm, unit, origin=origin, verbose=verbose, **kwargs)
+            self.mol.charge = charge
         return dip
 
     def to_gpu(self):
@@ -1434,7 +1442,26 @@ class HF(scf.hf.SCF):
 
     def dip_moment(self, mol=None, dm=None, unit='Debye', origin=None,
                    verbose=logger.NOTE, **kwargs):
-        raise TypeError('Use CDFT to calculate total dipole moment.') # CDFT will have this
+        if mol is None: mol = self.mol
+        if dm is None: dm = self.make_rdm1()
+        log = logger.new_logger(mol, verbose)
+
+        # Suppress warning about nonzero charge (if neutral)
+        charge = self.components['e'].mol.charge
+        self.components['e'].mol.charge = self.mol.charge
+        el_dip = self.components['e'].dip_moment(mol.components['e'],
+                                                 dm['e'], unit=unit,
+                                                 origin=origin, verbose=verbose-1)
+        self.components['e'].mol.charge = charge
+
+        # Quantum nuclei
+        nucl_dip = 0
+        for t, comp in self.components.items():
+            if t.startswith('n'):
+                nucl_dip += comp.dip_moment(mol.components[t], dm[t], unit=unit, origin=origin, verbose=verbose-1)
+        mol_dip = nucl_dip + el_dip
+        log.note('Dipole moment(X, Y, Z, Debye): %8.5f, %8.5f, %8.5f', *mol_dip)
+        return mol_dip
 
     def quad_moment(self, mol=None, dm=None, unit='DebyeAngstrom', origin=None,
                     verbose=logger.NOTE, **kwargs):
